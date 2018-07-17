20625184dc2442cbf7269c7a7a198cee2a72a6dd
KhachikHarutyunyan <khachik789@gmail.com>
2018-07-17 22:22:23 +0400

    pictur

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  // tslint:disable-next-line:max-line-length
+  // https://themeforest.net/item/soup-restaurant-with-online-ordering-system-template/19719445?s_rank=32&_ga=2.99811806.1505492418.1531838649-1378379996.1531590005
 }
